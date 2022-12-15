6a21924a6ceac7078bd61dcd5c288b94ddcfdd48
santoshmondal <santosh.ece06@gmail.com>
2022-12-15 11:24:23 +0530

    initial commit.

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { sum } from "./mymath.js";
+import { mul } from "./mymath.js";
+
+function main() {
+  let output = sum(1, 1);
+  console.log("Output", output);
+
+  let output1 = mul(2, 5);
+  console.log("Mul", output1);
+}
+
+main();
